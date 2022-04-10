d0c5e19b0b71ada8bb1287e8a8586d1b6ffcc01e
kaveenakash <kaveenakashya@gmail.com>
2022-04-11 00:48:22 +0530

    date picker bug fixed

--- lib/screens/Profiles/add_user.dart
+++ lib/screens/Profiles/add_user.dart
@@ -152,7 +152,7 @@ class _AddReminderPageState extends State<AddUserPage> {
     DateTime? _pickerDate = await showDatePicker(
         context: context,
         initialDate: DateTime.now(),
-        firstDate: DateTime(2015),
+        firstDate: DateTime(1990),
         lastDate: DateTime(2121));
 
     if (_pickerDate != null) {
